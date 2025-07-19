2aa01b80a9a300d56ca03b6b76eab6da22bbee54
Abdulla Alshamsi <abdualsh@lab1r1s6.42abudhabi.ae>
2025-07-19 16:24:22 +0400

    update10

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abdualsh <abdualsh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 07:44:17 by abdualsh          #+#    #+#             */
-/*   Updated: 2025/07/19 16:17:31 by abdualsh         ###   ########.fr       */
+/*   Updated: 2025/07/19 16:24:03 by abdualsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int			ft_isalnum(int c);
 int			ft_isascii(int c);
 int			ft_isprint(int c);
 size_t		ft_strlen(const char *s);
-void		*ft_memset(void *b, int c, size_t len);
+void		*ft_memset(void *s, int c, size_t n);
 void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
