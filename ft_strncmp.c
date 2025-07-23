d7791810f38c1cba701b597fc1c67e4e020f1c71
Abdulla Alshamsi <abdualsh@lab1r1s6.42abudhabi.ae>
2025-07-23 13:13:19 +0400

    strncmp update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: abdualsh <abdualsh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 15:53:52 by abdualsh          #+#    #+#             */
-/*   Updated: 2025/07/17 15:56:11 by abdualsh         ###   ########.fr       */
+/*   Updated: 2025/07/23 13:12:19 by abdualsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	x;
-
-	x = 0;
-	while (x < n && s1[x] == s2[x] && s1[x] != '\0'
-		&& s2[x] != '\0')
-		x++;
-	if (x == n)
+	while (*s1 && *s2 && n > 0)
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+		n--;
+	}
+	if (n == 0)
 		return (0);
-	return ((unsigned char)s1[x] - (unsigned char)s2[x]);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
