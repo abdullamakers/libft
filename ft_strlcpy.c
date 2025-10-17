9e8b3d40bde6c572c9b74756c05d518f8ee36dce
Abdulla Alshamsi <abdualsh@lab1r1s6.42abudhabi.ae>
2025-10-17 09:54:08 +0400

    ft_strlcpy.c have problem is dstsize - 1 have bug if dstsize error

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: abdualsh <abdualsh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 07:42:16 by abdualsh          #+#    #+#             */
-/*   Updated: 2025/07/21 15:23:17 by abdualsh         ###   ########.fr       */
+/*   Updated: 2025/10/17 09:53:01 by abdualsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
+	size_t	src_dan;
+	size_t	src_len;
 
-	if (!dst || !src)
-		return (0);
-	i = 0;
-	if (dstsize > 0)
+	src_len = 0;
+	src_dan = 0;
+	while (src[src_len])
+		src_len++;
+	if (dstsize == 0)
+		return (src_len);
+	while (src[src_dan] && (dstsize - 1 > src_dan))
 	{
-		while (src[i] && i < dstsize - 1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		dst[src_dan] = src[src_dan];
+		src_dan++;
 	}
-	while (src[i])
-		i++;
-	return (i);
+	if (dst[src_dan] != '\0')
+		dst[src_dan] = '\0';
+	return (src_len);
 }
