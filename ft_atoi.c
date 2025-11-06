bdc5a5ef842e28edfa73dbe684b795236be020cb
abdullaalshamsi <abdullamaker2022@gmail.com>
2025-11-07 00:38:41 +0400

    change a atoi only this a long_max

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: server-2 <server-2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 11:46:02 by abdualsh          #+#    #+#             */
-/*   Updated: 2025/11/02 20:18:31 by server-2         ###   ########.fr       */
+/*   Updated: 2025/11/07 00:32:55 by server-2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	ft_atoi(const char *s)
 {
-	int		i;
-	int		sign;
-	long	result;
+	unsigned int	i;
+	long			sign;
+	long			result;
 
 	i = 0;
 	sign = 1;
 	result = 0;
-	while ((s[i] >= 9 && s[i] <= 13) || s[i] == 32)
+	while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
 		i++;
 	if (s[i] == '-' || s[i] == '+')
 	{
@@ -31,8 +31,12 @@ int	ft_atoi(const char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
+		if (result >= LONG_MAX / 10 && sign == 1)
+			return (-1);
+		else if (result >= LONG_MAX / 10 && sign == -1)
+			return (0);
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
-	return ((int)(result * sign));
+	return ((int)result * sign);
 }
